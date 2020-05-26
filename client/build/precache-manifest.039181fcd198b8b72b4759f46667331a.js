06453b007d97912ad9fbfa5f1cb53317dd9c85cd
robynp108 <robynp108@gmail.com>
2020-05-26 11:39:27 -0500

    debug heroku deploy, move files outside of create-react-express

---
+++ client/build/precache-manifest.039181fcd198b8b72b4759f46667331a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ee1b216f1a567485d0f",
+    "url": "/static/css/main.83b5e304.chunk.css"
+  },
+  {
+    "revision": "7ee1b216f1a567485d0f",
+    "url": "/static/js/main.47ccfc20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07c1d4849373f8d80317",
+    "url": "/static/js/2.1602a2bc.chunk.js"
+  },
+  {
+    "revision": "b6fbdba63ef4b9e6bc3add0e62605127",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+// const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+const app = express();
+
+// app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds115595.mlab.com:15595/heroku_tsxmp9w7"
+mongoose.connect(MONGODB_URI);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+app.get("/concerns", (req, res) => {
+  db.Concern.find({}, (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.json(data);
+      }
+  });
+});
+
+app.put("/concerns/:id", (req, res) => {
+  db.Concern.findById(req.params.id, async (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      data.last_check = req.body.last_check;
+      await data.save();
+      res.json(data);
+    }
+  });
+  // const mongooseId = new mongoose.Types.ObjectId(req.params.id);
+  // db.Concern.findByIdAndUpdate(req.params.id, {$set: {last_check: req.body.last_check}});
+  // res.send(req.body.last_check);
+});
+
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
