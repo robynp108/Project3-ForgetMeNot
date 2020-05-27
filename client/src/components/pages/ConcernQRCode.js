42bb1c2e54ad627a6753255989142fe3a14c63f8
robynp108 <robynp108@gmail.com>
2020-05-27 18:41:59 -0500

    qr code generated with concern id

--- client/src/components/pages/ConcernQRCode.js
+++ client/src/components/pages/ConcernQRCode.js
@@ -1,30 +1,29 @@
-// import "./project3.css";
-
 var React = require('react');
 var QRCode = require('qrcode.react');
 
 function ConcernQRCode(props) {
     console.log(props);
     const concernName = props.match.params.name;
+    const concernId = props.match.params.id;
+
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
+    const port = window.location.port;
 
     return (
-        <div className="field is-grouped is-grouped-centered">
-            <p class="title is-2 is-spaced">{concernName}</p>
-            <br />
-            <div className="box">
-                <QRCode value="https://www.google.com" />
+        <div class="columns">
+            <div class="column">
+                <p class="title is-2 is-spaced">{concernName}</p>
             </div>
+            <div class="column is-2 is v-centered">
+                <div className="box">
+                    <QRCode value={protocol + "//" + hostname + ":" + port + "/landingpage/" + concernId} />
+                </div>
+            </div>
+            <div class="column"></div>
+            <div class="column"></div>
         </div>
     );
 }
 
 export default ConcernQRCode;
-
-// React.render(
-//   <QRCode value="http://facebook.github.io/react/" />,
-//   mountNode
-// );
-
-// return (
-//     <li><a href={"https://forget-me-not-project-3.herokuapp.com/qrcode" + props.id} target="_blank">QR Code</a>{props.name}, {props.last_check}</li>
-// );
