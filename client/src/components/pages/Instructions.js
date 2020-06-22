4fead3b55ce3ebdf2f75bacda44884f78e7ccf29
robynp108 <robynp108@gmail.com>
2020-06-22 12:03:58 -0500

    added rear facing camera default and fixed navbar

--- client/src/components/pages/Instructions.js
+++ client/src/components/pages/Instructions.js
@@ -11,17 +11,18 @@ function Home() {
                 <div className="box" style={{ width: "50%", marginLeft: "25px" }}>
                     <ul>
                         <li style={{ fontFamily: "'Kalam', cursive" }}>INSTRUCTIONS</li>
+                        <li>(You will need to be on a device connected to a printer for step 5.)</li>
                         <li>1. Sign up for an account, then login</li>
                         <li>2. Start creating your list of concerns! Navigate to the "manage list" page. Add your concerns with names like “Oven off” and “Took
                             morning meds”.</li>
                         <li>3. The concern will appear on the list with a link to a QR Code on the left and an "X" to delete if desired.</li>
                         <li>4. Click “QR Code” to see a page with that concern’s QR Code.</li>
                         <li>5. Print the code and tape it on or near the item of concern.</li>
-                        <li>6. Whenever you do the task, just scan the code with a QR scanner on your phone, go to that
+                        <li>6. Whenever you do the task, just scan the code with our built-in QR scanner, go to that
                             website, and you’ll see a page telling you the scan was successful.</li>
                         <li>7. Use the Home button to return to your Home page and you will see the time recorded on
                             your home page list!</li>
-                        <li>8. Stay logged in to quickly see your list anytime.</li>
+                        <li>8. Leave a window open on the home page to quickly see your list anytime.</li>
                     </ul>
                 </div>
             </div>
@@ -30,9 +31,9 @@ function Home() {
                 <div className="box" style={{ width: "50%", marginLeft: "25px" }}>
                     <ul>
                         <li style={{ fontFamily: "'Kalam', cursive" }}>SAMPLE CONCERN LIST</li>
-                        <li>“QR Code” Oven off, Wed, May 27th 2020, 6:10:01 pm</li>
-                        <li>“QR Code” Pantry closed, Wed, May 27th 2020, 10:54:25 am</li>
-                        <li>“QR Code” Front door locked, Wed, May 27th 2020, 5:50:06 pm</li>
+                        <li><strong>Oven off</strong>: Wed, May 27th 2020, 6:10:01 pm</li>
+                        <li><strong>Took morning meds</strong>: Wed, May 27th 2020, 10:54:25 am</li>
+                        <li><strong>Front door locked</strong>: Wed, May 27th 2020, 5:50:06 pm</li>
                     </ul>
                 </div>
             </div>
